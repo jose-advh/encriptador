6d8497003879b4df5f2a99929d28d9287dbd64ad
Sala de Sistemas 1-6 / Sede 1 <sala161@CIACADEMICO.LOCAL>
2024-08-12 11:23:54 -0500

    v0.8.8

--- scripts.js
+++ scripts.js
@@ -1,35 +1,46 @@
-    let mensajesOriginales = [];
-    let mensajesEncriptados = [];
+    let mensajesOriginales = '';
+    let mensajesEncriptados = '';
 
 document.addEventListener('DOMContentLoaded', (event) => {
     const textarea = document.getElementById('textarea');
     const encriptar = document.getElementById('encriptar');
+    const desencriptar = document.getElementById('desencriptar')
     const encriptados = document.getElementById('encriptados');
     const munecoLupa = document.getElementById('munecoLupa');
     const contador = document.getElementById('contador');
     const texto = document.getElementById('copyButton').innerHTML;
 
-    
 
     encriptar.addEventListener('click', () => {
         let mensaje = textarea.value;
         if (mensaje) {
-            mensajesOriginales.push(mensaje);
+            mensajesOriginales = mensaje;
             mensaje = mensaje.replace(/e/g, 'enter')
                              .replace(/i/g, 'imes')
                              .replace(/a/g, 'ai')
                              .replace(/o/g, 'ober')
                              .replace(/u/g, 'ufat');
-            mensajesEncriptados.push(mensaje);
+            mensajesEncriptados = mensaje;
             textarea.value = "";
             console.log(mensajesOriginales);
             actualizarMensajes();
         }
     });
 
+    desencriptar.addEventListener('click', () => {
+        mensaje = textarea.value;
+        if (mensaje === mensajesEncriptados) {
+            encriptados.innerHTML = mensajesOriginales;
+            textarea.value = "";
+        } else {
+            
+        }
+
+    })
+
     function actualizarMensajes() {
         if (mensajesEncriptados.length > 0) {
-            encriptados.innerHTML = mensajesEncriptados.join('<br>');
+            encriptados.innerHTML = mensajesEncriptados;
             toggleVisibility(true)
         } else {
             encriptados.textContent = 'Ningún mensaje fue encontrado';
@@ -57,7 +68,7 @@ function actualizarContadorMax() {
 
 const copiarContenido = async () => {
     try {
-        const ultimoMensaje = mensajesEncriptados[mensajesEncriptados.length - 1];
+        const ultimoMensaje = mensajesEncriptados;
         await navigator.clipboard.writeText(ultimoMensaje);
         console.log("Funcionó")
     } catch (err) {
